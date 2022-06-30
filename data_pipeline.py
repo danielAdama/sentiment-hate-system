02a326cbd23d380cc78bca5e6dc5c7fb75c15b05
danielAdama <adamadaniel321@gmail.com>
2022-06-30 19:23:21 +0100

    Pipeline to extract and store tweets in a MySQL database

---
+++ data_pipeline.py
@@ -0,0 +1,11 @@
+import numpy
+import pandas
+import mysql.connector
+import tweepy as tw
+from config import config
+
+
+consumer_key = config.APIKEY
+consumer_secret = config.APIKEYSECRET
+access_token = config.ACCESSTOKEN
+access_token_secret = config.ACCESSTOKENSECRET
