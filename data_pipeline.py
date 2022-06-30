4ccb724dea7f8b4835dc9aef34716bd4f21ee720
danielAdama <adamadaniel321@gmail.com>
2022-06-30 19:47:51 +0100

    Pipeline to extract and store tweets in a MySQL database

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,3 +1,4 @@
+from reprlib import aRepr
 import numpy
 import pandas
 import mysql.connector
@@ -9,3 +10,20 @@ consumer_key = config.APIKEY
 consumer_secret = config.APIKEYSECRET
 access_token = config.ACCESSTOKEN
 access_token_secret = config.ACCESSTOKENSECRET
+
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+ 
+
+# According to research, Canadian cities with the highest black population with experience 
+# racism the most are Toronto, Montreal, Edmonton, Calgary, Winnipeg and Vancouver.
+hashtags = [
+    '#toronto', '#edmonton', '#calgary', '#winnipeg', '#montreal', '#vancouver', '#studiolife', 
+    '#aislife', '#requires', '#passions', '#white', '#supremacists', '#inthefeels', 
+    '#deep', '#politics', '#blm', '#brexit', '#trump', '#music'
+    ]
+
+for hashtag in hashtags:
+    query = tw.Cursor(api.search_tweets, q=hashtag)
+    
