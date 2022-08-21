3022f8cad3e81efe314ca2fd868689313a611aaf
danielAdama <adamadaniel321.com>
2022-08-21 13:05:55 +0100

    Test for components in Modelinference Module

--- src/tests/test_modelinference.py
+++ src/tests/test_modelinference.py
@@ -1,9 +1,6 @@
 import pytest
-import pandas as pd
 import numpy as np
-from nltk import sent_tokenize
 from pipeline.modelinference import ModelInference
-from config import config
 
 mi = ModelInference('vectorizerV3.bin', 'modelV3.bin')
 
@@ -23,23 +20,6 @@ def test_count_pos_tag():
     
     assert fun == expect
 
-@pytest.fixture
-def dummy_data():
-    data = pd.DataFrame({
-    "id":[0, 1, 2],
-    "text":
-    ["Cause cause because YOU",
-    "cause me @danieltovia1 and @user get to live together for a whole week!   #cantwaittocook ðð",
-    "half way through the website now and #allgoingwell very  "
-    ]
-    })
-    return data
-
-@pytest.fixture
-def get_make_features_data_type(dummy_data):
-    actual_dtype = mi.make_features(dummy_data).dtypes.to_dict()
-    return actual_dtype
-
 def test_make_feature_data_type(get_make_features_data_type):
     expected_dtype = {
         'mention_count':np.dtype('int64'),
@@ -55,54 +35,12 @@ def test_make_feature_data_type(get_make_features_data_type):
     }
     assert get_make_features_data_type == expected_dtype
 
-@pytest.fixture
-def get_make_features_columns():
-    data = pd.DataFrame({
-    "id":[0, 1, 2],
-    "text":["Cause cause because YOU",
-    "cause me @danieltovia1 and @user get to live together for a whole week!   #cantwaittocook ðð",
-    "half way through the website now and #allgoingwell very  "]
-    })
-    actual_columns = mi.make_features(data).columns.tolist()
-    return actual_columns
-
 def test_make_feature_schema(get_make_features_columns):
     expected_schema = ['mention_count', 'char_count', 'word_count', 'uniq_word_count', 'htag_count', 
     'stopword_count', 'sent_count', 'avg_word_len','avg_sent_len', 'uniq_vs_words', 'stopwords_vs_words',
     'title_word_count', 'uppercase_count','noun_count', 'verb_count', 'adj_count', 
     'adv_count', 'pron_count', 'cleaned_text']
     assert get_make_features_columns == expected_schema
-
-@pytest.fixture
-def expect_df():
-    df = pd.DataFrame([])
-    df['id'] = [0, 1, 2]
-    df['text'] = [
-        "Cause cause because YOU",
-        "cause me @danieltovia1 and @user get to live together for a whole week!   #cantwaittocook ðð",
-        "half way through the website now and #allgoingwell very  "
-    ]
-    df['mention_count'] = [0, 2, 0]
-    df['char_count'] = [23, 80, 57]
-    df['word_count'] = [4, 13, 9]
-    df['uniq_word_count'] = [4, 13, 9]
-    df['htag_count'] = [0, 1, 1]
-    df['stopword_count'] = [1, 5, 5]
-    df['sent_count'] = [1, 2, 1]
-    df['avg_word_len'] = [4.6, 5.6, 5.7]
-    df['avg_sent_len'] = [2.0, 4.7, 4.5]
-    df['uniq_vs_words'] = [1.0, 1.0, 1.0]
-    df['stopwords_vs_words'] = [0.25, 0.35, 0.56]
-    df['title_word_count'] = [1, 0, 0]
-    df['uppercase_count'] = [1, 0, 0]
-    df['noun_count'] = [0, 3, 3]
-    df['verb_count'] = [0, 2, 0]
-    df['adj_count'] = [0, 1, 0]
-    df['adv_count'] = [1, 1, 3]
-    df['pron_count'] = [1, 1, 0]
-    df['cleaned_text'] = ["", "live week   canotwaittocook ", "half website  allgoingwell"]
-
-    return df
 
 def test_make_features_integer(dummy_data, expect_df):
     expect_df_int = expect_df[['mention_count', 'char_count', 'word_count', 'uniq_word_count', 'htag_count', 
